7371212a1c4246445cc0bc01a6d8ca095f7a2f15
Huevang Niashoua <91864265>
2024-05-03 16:50:58 +0700

    add app.js to github

---
+++ app.js
@@ -0,0 +1,20 @@
+// console.log('hello node.js'+ process.argv[2]+'!')
+
+
+var firstname= 'huevnag';
+var lastname= 'vang';
+
+console.log(firstname+" and lastname:"+lastname)
+
+let classname = 'mysql';
+console.log(classname)
+// let classname= 'text';
+
+const text = 'test';
+text = 'home'
+console.log(text);
+const i = 5;
+if(i>3){
+    let text='good moring'
+    console.log(text)
+}
