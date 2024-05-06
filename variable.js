c6e2b16126b1935ad1d80ed3789714b86699fc65
Huevang Niashoua <91864265>
2024-05-06 15:51:12 +0700

    add variable

--- variable.js
+++ variable.js
@@ -6,3 +6,27 @@ console.log(blue); // 255 ເປັນເລກຖານ 10
 
 let pi = 3.14159;
 console.log(pi);
+
+let firstname = " huevang";
+let lastname = "vang";
+console.log(firstname +"and lastname:"+lastname);
+
+let isReading = true;
+ let isSleeping = false;
+ console.log(isReading);
+ console.log(isSleeping);
+
+ let data = null;
+ console.log(data);
+
+ let i
+  
+ console.log(i)
+
+ let sym1 = Symbol('foo');
+ console.log(sym1);
+
+ let mycar = new Object();
+ mycar.make = 'ford';
+ mycar.model = 'Mustang';
+ console.log(mycar.make);
